d95a1e1039437f6716e99553da436ed64fac0cef
Joshua Schmidt <jschmidtnj@gmail.com>
2018-05-23 16:16:58 -0500

    added small changes - two scripts are now incompatible

--- motiondetect.py
+++ motiondetect.py
@@ -32,16 +32,6 @@ camera = PiCamera()
 camera.resolution = (1920,1080)
 camera.framerate = 30 #16
 rawCapture = PiRGBArray(camera, size=(1920,1080))
-output = np.empty((1000, 1000, 3), dtype=np.uint8)
-
-# Load a sample picture and learn how to recognize it.
-print("Loading known face image(s)")
-obama_image = face_recognition.load_image_file("obama_small.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-
-# Initialize some variables
-face_locations = []
-face_encodings = []
 
 
 # allow the camera to warmup, then initialize the average frame, last
@@ -62,7 +52,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 
 	# resize the frame, convert it to grayscale, and blur it
 	#frame=500 default, decrease it to go faster
-	frame = imutils.resize(frame, width=500)
+	frame = imutils.resize(frame, width=1000)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -127,24 +117,6 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 				lastUploaded = timestamp
 				motionCounter = 0
 				text=""
-
-				print("Capturing image.")
-				# Grab a single frame of video from the RPi camera as a numpy array
-				camera.capture(output, format="rgb")
-				# Find all the faces and face encodings in the current frame of video
-				face_locations = face_recognition.face_locations(output)
-				print("Found {} faces in image.".format(len(face_locations)))
-				face_encodings = face_recognition.face_encodings(output, face_locations)
-
-				# Loop over each face found in the frame to see if it's someone we know.
-				for face_encoding in face_encodings:
-					# See if the face is a match for the known face(s)
-					match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
-					name = "<Unknown Person>"
-
-					if match[0]:
-            					name = "Barack Obama"
-					print(("found someone named {}!").format(name))
 
 	# otherwise, the room is not occupied
 	else:
