372f61573c8d90c77b7ab57463eea9b3e4ebf91c
Joshua Schmidt <jschmidtnj@gmail.com>
2018-05-25 12:59:20 -0400

    Update motiondetect.py

--- motiondetect.py
+++ motiondetect.py
@@ -1,6 +1,5 @@
 from pyimagesearch import *
 from pyimagesearch.tempimage import TempImage
-import dropbox as dbx
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import warnings
@@ -10,158 +9,112 @@ import json
 import time
 import cv2
 import os
-import face_recognition
 import numpy as np
 import glob
+import dropboximage
 
-# filter warnings, load the configuration and initialize the Dropbox
-warnings.filterwarnings("ignore")
+def main():
+	# filter warnings, load the configuration and initialize the Dropbox
+	warnings.filterwarnings("ignore")
 
-# Put your token here:
-with open("permissions.json") as f:
-	data = json.load(f)
-client = dbx.Dropbox(data['db-token'])
+	# initialize the camera and grab a reference to the raw camera capture
+	camera = PiCamera()
+	#default 640x480 - decrease to go faster
+	#motion-detect camera resolution
+	camera.resolution = (640,480)
+	rawCapture = PiRGBArray(camera, size=(640,480))
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-#default 640x480 - decrease to go faster
-#motion-detect camera resolution
-camera.resolution = (640,480)
-rawCapture = PiRGBArray(camera, size=(640,480))
+	# allow the camera to warmup, then initialize the average frame, last
+	# uploaded timestamp, and frame motion counter
+	print("[INFO] warming up...")
+	time.sleep(2.5)
+	avg = None
+	lastUploaded = datetime.datetime.now()
+	motionCounter = 0
+	text = ""
+	name = ""
 
-#facial-detect camera
-output = np.empty((240, 320, 3), dtype=np.uint8)
+	# capture frames from the camera
+	for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	    # grab the raw NumPy array representing the image and initialize
+		# the timestamp and occupied/unoccupied text
+		frame = f.array
+		timestamp = datetime.datetime.now()
 
-# Load a sample picture and learn how to recognize it.
-print("Loading known face image(s)")
-images = []
-encoding = []
-os.chdir("images")
-for file in glob.glob("*.jpg"):
-	face_recognition.load_image-file(file)
-	faces.append(file)
-	encoding.append(face_recognition.face_encodings(file)[0])
+		# resize the frame, convert it to grayscale, and blur it
+		#frame=500 default, decrease it to go faster
+		frame = imutils.resize(frame, width=500)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
-
-# allow the camera to warmup, then initialize the average frame, last
-# uploaded timestamp, and frame motion counter
-print("[INFO] warming up...")
-time.sleep(2.5)
-avg = None
-lastUploaded = datetime.datetime.now()
-motionCounter = 0
-text = ""
-name = ""
-
-# capture frames from the camera
-for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image and initialize
-	# the timestamp and occupied/unoccupied text
-	frame = f.array
-	timestamp = datetime.datetime.now()
-
-	# resize the frame, convert it to grayscale, and blur it
-	#frame=500 default, decrease it to go faster
-	frame = imutils.resize(frame, width=500)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-	# if the average frame is None, initialize it
-	if avg is None:
-		print("[INFO] starting background model...")
-		avg = gray.copy().astype("float")
-		rawCapture.truncate(0)
-		continue
-
-	# accumulate the weighted average between the current frame and
-	# previous frames, then compute the difference between the current
-	# frame and running average
-	cv2.accumulateWeighted(gray, avg, 0.5)
-	frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-
-	# threshold the delta image, dilate the thresholded image to fill
-	# in holes, then find contours on thresholded image
-	thresh = cv2.threshold(frameDelta, 5, 255, cv2.THRESH_BINARY)[1]
-	thresh = cv2.dilate(thresh, None, iterations=2)
-	(_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-	# loop over the contours
-	for c in cnts:
-		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < 5000:
+		# if the average frame is None, initialize it
+		if avg is None:
+			print("[INFO] starting background model...")
+			avg = gray.copy().astype("float")
+			rawCapture.truncate(0)
 			continue
 
-		# compute the bounding box for the contour, draw it on the frame,
-		# and update the text
-		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		text = "!"
+		# accumulate the weighted average between the current frame and
+		# previous frames, then compute the difference between the current
+		# frame and running average
+		cv2.accumulateWeighted(gray, avg, 0.5)
+		frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
-	# draw the text and timestamp on the frame
-	ts = timestamp.strftime("%A_%d_B_%Y_%I:%M:%S%p")
-	cv2.putText(frame, "{}".format(ts), (10, 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+		# threshold the delta image, dilate the thresholded image to fill
+		# in holes, then find contours on thresholded image
+		thresh = cv2.threshold(frameDelta, 5, 255, cv2.THRESH_BINARY)[1]
+		thresh = cv2.dilate(thresh, None, iterations=2)
+		(_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-	# check to see if the room is occupied
-	if text == "!":
-		# check to see if enough time has passed between uploads
-		if (timestamp - lastUploaded).seconds >= 3.0:
-			# increment the motion counter
-			motionCounter += 1
+		# loop over the contours
+		for c in cnts:
+			# if the contour is too small, ignore it
+			if cv2.contourArea(c) < 5000:
+				continue
 
-			# check to see if the number of frames with consistent motion is
-			# high enough
-			if motionCounter >= 8: #originally 8
-				#run the facial recognition:
+			# compute the bounding box for the contour, draw it on the frame,
+			# and update the text
+			(x, y, w, h) = cv2.boundingRect(c)
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+			text = "!"
 
-				print("Capturing image.")
-				# Grab a single frame of video from the RPi camera as a numpy array
-				with camera as face_camera:
-					face_camera.resolution = (320, 240)
-					face_camera.capture(output, format="rgb")
-					face_camera.resolution = (640, 480)
+		# draw the text and timestamp on the frame
+		ts = timestamp.strftime("%A_%d_B_%Y_%I:%M:%S%p")
+		cv2.putText(frame, "{}".format(ts), (10, 20),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-				# Find all the faces and face encodings in the current frame of video
-				face_locations = face_recognition.face_locations(output)
-				print("Found {} faces in image.".format(len(face_locations)))
-				face_encodings = face_recognition.face_encodings(output, face_locations)
+		# check to see if the room is occupied
+		if text == "!":
+			# check to see if enough time has passed between uploads
+			if (timestamp - lastUploaded).seconds >= 3.0:
+				# increment the motion counter
+				motionCounter += 1
 
-				# Loop over each face found in the frame to see if it's someone we know.
-				for face_encoding in face_encodings:
-					# See if the face is a match for the known face(s)
-					match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
-					name = "!"
-
-					if match[0]:
-						name = "Barack Obama"
-
-				if name != "!":
-					print("I see someone named {}!".format(name))
-				else:
-					print("Intruder!")
-					print("[UPLOAD] {}".format(ts))
+				# check to see if the number of frames with consistent motion is
+				# high enough
+				if motionCounter >= 8: #originally 8
+					print("Capturing image.")
+					t = TempImage()
+					cv2.imwrite(t.path, frame)
 					name = "{base}{timestamp}".format(base="", timestamp=ts)
-					with camera as good_camera:
-						good_camera.resolution = (1920, 1080)
-						good_camera.capture("{}.jpg".format(name))
-						good_camera.resolution = (640, 480)
-					with open("/home/pi/Desktop/pisecuritysystem/{name}.jpg".format(name=name), "rb") as f:
-						client.files_upload(f.read(), "/{name}.jpg".format(name=name), mute = True)
+					os.rename(t.path[3:], "{new}.jpg".format(new=name))
 					os.remove("{name}.jpg".format(name=name))
+					#optional temporary file above
+					#run the gropboximage.py file here:
+					dropboximage.main()
+					# update the last uploaded timestamp and reset the motion
+					# counter
+					lastUploaded = timestamp
+					motionCounter = 0
+					text=""
 
-				# update the last uploaded timestamp and reset the motion
-				# counter
-				lastUploaded = timestamp
-				motionCounter = 0
-				text=""
+		# otherwise, the room is not occupied
+		else:
+			motionCounter = 0
+			text=""
 
-	# otherwise, the room is not occupied
-	else:
-		motionCounter = 0
-		text=""
+		# clear the stream in preparation for the next frame
+		rawCapture.truncate(0)
 
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
+if __name__ == '__main__':
+	main()
