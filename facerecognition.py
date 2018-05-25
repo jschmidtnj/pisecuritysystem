87369a29f98d966dc98b49923fcff466f2d4929e
Joshua Schmidt <jschmidtnj@gmail.com>
2018-05-25 12:57:34 -0400

    Update facerecognition.py

--- facerecognition.py
+++ facerecognition.py
@@ -15,50 +15,56 @@ import time
 import datetime
 import glob
 import os
+import dropboximage
 
-# Get a reference to the Raspberry Pi camera.
-# If this fails, make sure you have a camera connected to the RPi and that you
-# enabled your camera in raspi-config and rebooted first.
-camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-output = np.empty((240, 320, 3), dtype=np.uint8)
+main():
+	# Get a reference to the Raspberry Pi camera.
+	# If this fails, make sure you have a camera connected to the RPi and that you
+	# enabled your camera in raspi-config and rebooted first.
+	camera = picamera.PiCamera()
+	camera.resolution = (320, 240)
+	output = np.empty((240, 320, 3), dtype=np.uint8)
 
-# Load a sample picture and learn how to recognize it.
-print("Loading known face image(s)")
-images = []
-encoding = []
-os.chdir("images")
-for file in glob.glob("*.jpg"):
-	face_recognition.load_image-file(file)
-	faces.append(file)
-	encodings.append(face_recognition.face_encodings(file[0]))
+	# Load a sample picture and learn how to recognize it.
+	print("Loading known face image(s)")
+	images = []
+	encoding = []
+	os.chdir("images")
+	for file in glob.glob("*.jpg"):
+		face_recognition.load_image-file(file)
+		faces.append(file)
+		encodings.append(face_recognition.face_encodings(file[0]))
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
+	# Initialize some variables
+	face_locations = []
+	face_encodings = []
 
-while True:
-    print("Capturing image.")
-    # Grab a single frame of video from the RPi camera as a numpy array
-    camera.capture(output, format="rgb")
+	while True:
+	    print("Capturing image.")
+	    # Grab a single frame of video from the RPi camera as a numpy array
+	    camera.capture(output, format="rgb")
 
-    # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(output)
-    print("Found {} faces in image.".format(len(face_locations)))
-    face_encodings = face_recognition.face_encodings(output, face_locations)
+	    # Find all the faces and face encodings in the current frame of video
+	    face_locations = face_recognition.face_locations(output)
+	    print("Found {} faces in image.".format(len(face_locations)))
+	    face_encodings = face_recognition.face_encodings(output, face_locations)
 
-    intruder = False
-    # Loop over each face found in the frame to see if it's someone we know.
-    for face_encoding in face_encodings:
-	# See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces(encodings, face_encoding)
+	    intruder = False
+	    # Loop over each face found in the frame to see if it's someone we know.
+	    for face_encoding in face_encodings:
+		# See if the face is a match for the known face(s)
+		match = face_recognition.compare_faces(encodings, face_encoding)
 
-        if match[0] == False:
-            intruder = True
+		if match[0] == False:
+			intruder = True
 
-        if intruder:
-            print("I see an Intruder!")
-        #run the send to dropbox script
-        else:
-            print("Everyone is friendly!")
+		if intruder:
+			print("I see an Intruder!")
+			#run the send to dropbox script
+			dropboximage.main()
+
+		else:
+			print("Everyone is friendly!")
+
 if __name__ == '__main__':
+	main()
