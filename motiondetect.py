45d709bceb82d65a6d7dd051980c425d0bf6cac2
Joshua Schmidt <jschmidtnj@gmail.com>
2018-05-25 19:23:44 -0400

    Update motiondetect.py

--- motiondetect.py
+++ motiondetect.py
@@ -16,9 +16,23 @@ import RPi.GPIO as GPIO
 import sys
 
 def main():
+	break_script = False
 	# filter warnings, load the configuration and initialize the Dropbox
 	warnings.filterwarnings("ignore")
 
+	#setup gpio
+	def power_button_pressed(channel):
+		break_script = True
+		GPIO.remove_event_detect(17)
+	GPIO.setmode(GPIO.BCM)
+	# GPIO 23 & 17 set up as inputs, pulled up to avoid false detection.
+	# Both ports are wired to connect to GND on button press.
+	# So we'll be setting up falling edge detection for both
+	GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	#define mode number by push button gpio pins or flags or something
+	# when a falling edge is detected on port 17, regardless of whatever
+	# else is happening in the program, the function mode_button_pressed will be run
+	GPIO.add_event_detect(17, GPIO.FALLING, callback=power_button_pressed, bouncetime=300)
 	#dropbox:
 	with open("permissions.json") as f:
 		data = json.load(f)
@@ -122,6 +136,9 @@ def main():
 
 		# clear the stream in preparation for the next frame
 		rawCapture.truncate(0)
+		if break_script:
+			print("exiting motiondetect script")
+			break
 
 if __name__ == '__main__':
 	main()
