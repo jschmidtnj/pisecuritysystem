1630d5af69bbcf7f5a95c33425700fcf82a77716
Joshua Schmidt <jschmidtnj@gmail.com>
2018-05-25 13:04:30 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import facerecognition
+import motiondetect
+
+def main():
+  #define mode number by push button gpio pins or flags or something
+  mode = 0
+
+  if mode == 0:
+    #run face-recognition
+    facerecognition.main()
+  elif mode == 1:
+    #run motion-detect
+    motiondetect.main()
+  
+if __name__ == '__main__':
+  main()
